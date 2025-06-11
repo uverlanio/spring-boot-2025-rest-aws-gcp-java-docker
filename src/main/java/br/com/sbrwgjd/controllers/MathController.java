96b88c3febea374a38c47db97b6113093558535f
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2025-06-11 07:48:33 -0300

    commit

---
+++ src/main/java/br/com/sbrwgjd/controllers/MathController.java
@@ -0,0 +1,39 @@
+package br.com.sbrwgjd.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/math")
+public class MathController {
+
+    @GetMapping("sum/{num1}/{num2}")
+    public Double sum(
+            @PathVariable("num1") double num1,
+            @PathVariable("num2") double num2){
+        return num1 + num2;
+    }
+
+    @GetMapping("subtract/{num1}/{num2}")
+    public Double subtract(
+            @PathVariable("num1") double num1,
+            @PathVariable("num2") double num2){
+        return num1 - num2;
+    }
+
+    @GetMapping("multiply/{num1}/{num2}")
+    public Double multiply(
+            @PathVariable("num1") double num1,
+            @PathVariable("num2") double num2){
+        return num1 * num2;
+    }
+
+    @GetMapping("divide/{num1}/{num2}")
+    public Double divide(
+            @PathVariable("num1") double num1,
+            @PathVariable("num2") double num2){
+        return num2 != 0 ? num1 / num2 : null;
+    }
+}
