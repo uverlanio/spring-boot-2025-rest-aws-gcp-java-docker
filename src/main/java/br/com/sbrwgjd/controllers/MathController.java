1238e38fb82879f91fef5b197bb91665af08a1bd
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2025-06-11 07:53:30 -0300

    commit

--- src/main/java/br/com/sbrwgjd/controllers/MathController.java
+++ src/main/java/br/com/sbrwgjd/controllers/MathController.java
@@ -9,31 +9,43 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/math")
 public class MathController {
 
-    @GetMapping("sum/{num1}/{num2}")
-    public Double sum(
-            @PathVariable("num1") double num1,
-            @PathVariable("num2") double num2){
-        return num1 + num2;
-    }
-
     @GetMapping("subtract/{num1}/{num2}")
     public Double subtract(
             @PathVariable("num1") double num1,
-            @PathVariable("num2") double num2){
+            @PathVariable("num2") double num2) throws IllegalArgumentException {
+
+        if (!isNumeric(num1) || !isNumeric(num2)) {
+            throw new IllegalArgumentException();
+        }
+
         return num1 - num2;
     }
 
     @GetMapping("multiply/{num1}/{num2}")
     public Double multiply(
             @PathVariable("num1") double num1,
-            @PathVariable("num2") double num2){
+            @PathVariable("num2") double num2) throws IllegalArgumentException {
+
+        if (!isNumeric(num1) || !isNumeric(num2)) {
+            throw new IllegalArgumentException();
+        }
+
         return num1 * num2;
     }
 
     @GetMapping("divide/{num1}/{num2}")
     public Double divide(
             @PathVariable("num1") double num1,
-            @PathVariable("num2") double num2){
-        return num2 != 0 ? num1 / num2 : null;
+            @PathVariable("num2") double num2) throws IllegalArgumentException {
+
+        if (!isNumeric(num1) || !isNumeric(num2) || num2 == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        return num1 / num2;
+    }
+
+    private boolean isNumeric(double num) {
+        return !Double.isNaN(num) && !Double.isInfinite(num);
     }
 }
