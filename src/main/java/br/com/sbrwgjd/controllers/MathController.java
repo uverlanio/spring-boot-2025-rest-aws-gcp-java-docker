c19f440ff45df07421130d0c2ae4d8ba3830585a
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2025-06-16 07:43:26 -0300

    commit

--- src/main/java/br/com/sbrwgjd/controllers/MathController.java
+++ src/main/java/br/com/sbrwgjd/controllers/MathController.java
@@ -1,5 +1,6 @@
 package br.com.sbrwgjd.controllers;
 
+import br.com.sbrwgjd.exception.UnsupportedMathOperationException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +12,25 @@ import java.util.Objects;
 @RequestMapping("/math")
 public class MathController {
 
+    @GetMapping("sum/{num1}/{num2}")
+    public Double sum(
+            @PathVariable("num1") String num1,
+            @PathVariable("num2") String num2) throws IllegalArgumentException {
+
+        if (!isNumeric(num1) || !isNumeric(num2)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value.");
+        }
+
+        return convertToDouble(num1) + convertToDouble(num2);
+    }
+
     @GetMapping("subtract/{num1}/{num2}")
     public Double subtract(
             @PathVariable("num1") String num1,
             @PathVariable("num2") String num2) throws IllegalArgumentException {
 
         if (!isNumeric(num1) || !isNumeric(num2)) {
-            throw new IllegalArgumentException();
+            throw new UnsupportedMathOperationException("Please set a numeric value.");
         }
 
         return convertToDouble(num1) - convertToDouble(num2);
@@ -29,7 +42,7 @@ public class MathController {
             @PathVariable("num2") String num2) throws IllegalArgumentException {
 
         if (!isNumeric(num1) || !isNumeric(num2)) {
-            throw new IllegalArgumentException();
+            throw new UnsupportedMathOperationException("Please set a numeric value.");
         }
 
         return convertToDouble(num1) * convertToDouble(num2);
@@ -41,10 +54,33 @@ public class MathController {
             @PathVariable("num2") String num2) throws IllegalArgumentException {
 
         if (!isNumeric(num1) || !isNumeric(num2)) {
-            throw new IllegalArgumentException();
+            throw new UnsupportedMathOperationException("Please set a numeric value.");
         }
 
         return convertToDouble(num1) / convertToDouble(num2);
+    }
+
+    @GetMapping("mean/{num1}/{num2}")
+    public Double mean(
+            @PathVariable("num1") String num1,
+            @PathVariable("num2") String num2) throws IllegalArgumentException {
+
+        if (!isNumeric(num1) || !isNumeric(num2)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value.");
+        }
+
+        return (convertToDouble(num1) + convertToDouble(num2)) / 2;
+    }
+
+    @GetMapping("squareRoot/{num}")
+    public Double squareRoot(
+            @PathVariable("num") String num) throws IllegalArgumentException {
+
+        if (!isNumeric(num)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value.");
+        }
+
+        return Math.sqrt(convertToDouble(num));
     }
 
     private Double convertToDouble(String strNumber) {
