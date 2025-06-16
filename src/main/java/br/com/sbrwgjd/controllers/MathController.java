cc855e0dc3ed7cb6f08bf8f4b1085d4df76b1551
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2025-06-16 07:14:36 -0300

    commit

--- src/main/java/br/com/sbrwgjd/controllers/MathController.java
+++ src/main/java/br/com/sbrwgjd/controllers/MathController.java
@@ -5,47 +5,66 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 @RequestMapping("/math")
 public class MathController {
 
     @GetMapping("subtract/{num1}/{num2}")
     public Double subtract(
-            @PathVariable("num1") double num1,
-            @PathVariable("num2") double num2) throws IllegalArgumentException {
+            @PathVariable("num1") String num1,
+            @PathVariable("num2") String num2) throws IllegalArgumentException {
 
-        if (isNumeric(num1) || isNumeric(num2)) {
+        if (!isNumeric(num1) || !isNumeric(num2)) {
             throw new IllegalArgumentException();
         }
 
-        return num1 - num2;
+        return convertToDouble(num1) - convertToDouble(num2);
     }
 
     @GetMapping("multiply/{num1}/{num2}")
     public Double multiply(
-            @PathVariable("num1") double num1,
-            @PathVariable("num2") double num2) throws IllegalArgumentException {
+            @PathVariable("num1") String num1,
+            @PathVariable("num2") String num2) throws IllegalArgumentException {
 
-        if (isNumeric(num1) || isNumeric(num2)) {
+        if (!isNumeric(num1) || !isNumeric(num2)) {
             throw new IllegalArgumentException();
         }
 
-        return num1 * num2;
+        return convertToDouble(num1) * convertToDouble(num2);
     }
 
     @GetMapping("divide/{num1}/{num2}")
     public Double divide(
-            @PathVariable("num1") double num1,
-            @PathVariable("num2") double num2) throws IllegalArgumentException {
+            @PathVariable("num1") String num1,
+            @PathVariable("num2") String num2) throws IllegalArgumentException {
 
-        if (isNumeric(num1) || isNumeric(num2) || num2 == 0) {
+        if (!isNumeric(num1) || !isNumeric(num2)) {
             throw new IllegalArgumentException();
         }
 
-        return num1 / num2;
+        return convertToDouble(num1) / convertToDouble(num2);
     }
 
-    private boolean isNumeric(double num) {
-        return Double.isNaN(num) || Double.isInfinite(num);
+    private Double convertToDouble(String strNumber) {
+        if(strNumber == null || strNumber.isEmpty()){
+            throw new IllegalArgumentException();
+        }
+
+        String number = strNumber.replace(",", ".");
+        return Double.parseDouble(number);
+
+    }
+
+    private boolean isNumeric(String strNumber) {
+
+        if(strNumber == null || strNumber.isEmpty()){
+            return false;
+        }
+
+        String number = strNumber.replace(",", ".");
+
+        return number.matches("[-+]?[0-9]*\\.?[0-9+]");
     }
 }
