0d96a6e1cbc5deaf2d01dd59c28ee4f843e2eede
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2025-07-01 07:54:29 -0300

    commit

--- src/main/java/br/com/sbrwgjd/services/PersonServices.java
+++ src/main/java/br/com/sbrwgjd/services/PersonServices.java
@@ -8,13 +8,14 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class PersonServices {
 
     private final AtomicLong counter = new AtomicLong();
-    Logger logger = Logger.getLogger(PersonServices.class.getName());
+    Logger logger = LoggerFactory.getLogger(PersonServices.class.getName());
 
     @Autowired
     PersonRepository personRepository;
