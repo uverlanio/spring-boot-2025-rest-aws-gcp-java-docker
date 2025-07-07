2dd39c0873e99a744923b56dc9a9368136ffbe4e
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2025-07-07 18:58:36 -0300

    commit

---
+++ src/main/java/br/com/sbrwgjd/mapper/ObjectMapper.java
@@ -0,0 +1,31 @@
+package br.com.sbrwgjd.mapper;
+
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ObjectMapper {
+
+    private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+
+    public static <O, D> D parseObject(O origin, Class<D> destination){
+        return mapper.map(origin, destination);
+    }
+
+    public static <O, D> List<D> parseListObjects(List<O> origin, Class<D> destination){
+
+        List<D> destinationObjects = new ArrayList<D>();
+
+        origin.forEach(
+                o -> destinationObjects.add(mapper.map(o , destination))
+        );
+
+        /*for(Object o : origin){
+            destinationObjects.add(mapper.map(o, destination));
+        }*/
+        return destinationObjects;
+    }
+}
