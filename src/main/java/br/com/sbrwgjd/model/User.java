8a0c234adca25d6fb250b9b4499ba88a3cf9df99
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2026-03-18 17:48:58 -0300

    commit

---
+++ src/main/java/br/com/sbrwgjd/model/User.java
@@ -0,0 +1,75 @@
+package br.com.sbrwgjd.model;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.*;
+import org.springframework.security.core.userdetails.*;
+
+import java.io.*;
+import java.util.*;
+
+@Entity
+@Table(name = "user")
+public class User implements UserDetails, Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @Column
+    private String password;
+
+    @Column(name = "account_non_expired")
+    private Boolean accountNonExpired;
+
+    @Column(name = "account_non_locked")
+    private Boolean accountNonLocked;
+
+    @Column(name = "credentials_non_expired")
+    private Boolean credentialsNonExpired;
+
+    private Boolean enabled;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
