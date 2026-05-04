c7ee3d8473cab880dc19c9de0f41e33fe32caa3f
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2026-05-04 15:33:33 -0300

    commit

--- src/test/java/br/com/sbrwgjd/config/TestConfigs.java
+++ src/test/java/br/com/sbrwgjd/config/TestConfigs.java
@@ -2,7 +2,7 @@ package br.com.sbrwgjd.config;
 
 public interface TestConfigs {
 
-    int SERVER_PORT = 8888;
+    int SERVER_PORT = 8080;
     String HEADER_PARAM_AUTHORIZATION = "Authorization";
     String HEADER_PARAM_ORIGIN = "Origin";
 
